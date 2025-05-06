3449b269ac666cb1c2e30cf4c84158412f8e93e8
Pointyrook <erik.s.petrovich@gmail.com>
2025-05-05 22:03:49 -0700

    Labs 9, 10, 11 are WORKING

--- public/header.mjs
+++ public/header.mjs
@@ -76,5 +76,6 @@ darkModeCheckbox.addEventListener("change", (e) => {
 
 if (localStorage.getItem("dark-mode") === "true") {
     darkModeCheckbox.dispatchEvent(new Event("change"));
-    darkModeCheckbox.checked = true;
+    darkModeCheckbox.checked = true
+    console.log(darkModeCheckbox.checked);
 }
